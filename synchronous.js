8fbd0b1c03b9bac30e88dd0fd612dd5a68b213f5
Juan <jucsanch@riseup.net>
2017-11-21 14:55:39 -0800

    add instructions and delete unecessary lines

--- synchronous.js
+++ synchronous.js
@@ -1,13 +1,13 @@
+// Write a program that uses a single synchronous filesystem operation to
+// read a file and print the number of newlines (\n) it contains to the
+// console (stdout), similar to running cat file | wc -l.
+//
+// The full path to the file to read will be provided as the first
+// command-line argument (i.e., process.argv[2]). You do not need to make
+// your own test file.
+
 var fs      = require('fs'),
 fileString  = fs.readFileSync('./text.txt').toString().split('\n'),
-// fileString  = fs.readFileSync(process.argv[2]).toString().split('\n'),
 lines       = fileString.length - 1;
 
 console.log(lines)
-
-//no need for loop, just lenght it
-// for(var i = 0; i <= fileString.length - 2; i++){
-//   lineTotal ++;
-// }
-//
-// console.log(lineTotal);
